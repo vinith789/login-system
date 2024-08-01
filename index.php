04a3834e4a3f5a777cd5c2926b781c4e4b35f04d
vinith579 <vinithvinith356140@gmail.com>
2024-08-01 20:39:48 +0530

    index page up[date

--- index.php
+++ index.php
@@ -13,11 +13,6 @@
 
 </head>
 <body>
-
-<div class="form-container">
-
-   <form action="" method="post">
-      <h3>login now</h3>
       <?php
       if(isset($error)){
          foreach($error as $error){
@@ -25,13 +20,6 @@
          };
       };
       ?>
-      <input type="email" name="email" required placeholder="enter your email">
-      <input type="password" name="password" required placeholder="enter your password">
-      <input type="submit" name="submit" value="login now" class="form-btn">
-      <p>don't have an account? <a href="register_form.php">register now</a></p>
-   </form>
-
-</div>
 
 </body>
 </html>
